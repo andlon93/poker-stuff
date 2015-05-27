172d4545c0ee7c5dee8600591754496b56e0c73c
andlon93 <andreas.lones@gmail.com>
2015-05-27 17:36:59 +0200

    Handles multiple sessions on multiple files with different buy ins

--- andlon/MixGamesGraph.py
+++ andlon/MixGamesGraph.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt
 import sessionHandler as s
 
-debug = True
-
-sessions = s.get_sessions()
-
 def get_stack_from_input(line):
 	index = 17
 	while not line[index] == ' ': index += 1
@@ -17,16 +13,24 @@ def read_session_from_file(file_name):
 	buy_in = session[0]
 	session = [ round(stack - buy_in, 2) for stack in session ]
 	
-	if debug: print 'sesjon: ', session
 	file.close()
 	return session
 
-
+def read_all_sessions():
+	sessions = s.get_sessions()
+	all_hands = []
+	for session in sessions:
+		print session
+		if len(all_hands) == 0: all_hands += read_session_from_file(session)
+		else:
+			temp_result = all_hands[-1]
+			new_session = read_session_from_file(session)
+			all_hands += [round(hand+temp_result,2) for hand in new_session]
+	print 'Resultat: ', all_hands[-1], ' over ', len(all_hands), 'hender'
+	return all_hands
 
 def plot_graph(hands):
 	plt.plot(range(1,len(hands)+1),hands)
 	plt.show()
 
-plot_graph( read_session_from_file(sessions[0]) )
-
-
+plot_graph( read_all_sessions() )
