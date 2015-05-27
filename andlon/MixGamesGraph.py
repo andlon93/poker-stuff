6ab3331796a5abbca60fdf7b0c8b7732577a488f
andlon93 <andreas.lones@gmail.com>
2015-05-27 17:00:32 +0200

    improved

--- andlon/MixGamesGraph.py
+++ andlon/MixGamesGraph.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
 import sessionHandler as s
 
+debug = True
+
 sessions = s.get_sessions()
+
+def get_stack_from_input(line):
+	index = 17
+	while not line[index] == ' ': index += 1
+	return float(line[17:index])
 
 def read_session_from_file(file_name):
 	file = open(file_name,'r')
-	session = [10.0]
-	session += [ float( line[17:22] ) for line in file if line[8:17] == 'andlon ($' ]
+	session = [ get_stack_from_input(line) for line in file if line[8:17] == 'andlon ($' ]
+	
+	buy_in = session[0]
+	session = [ round(stack - buy_in, 2) for stack in session ]
+	
+	if debug: print 'sesjon: ', session
 	file.close()
 	return session
 
-def plot_graph(session):
-	plt.plot(range(1,len(session)+1),session)
+
+
+def plot_graph(hands):
+	plt.plot(range(1,len(hands)+1),hands)
 	plt.show()
 
-plot_graph( s.get_sessions() )
+plot_graph( read_session_from_file(sessions[0]) )
+
 
--- andlon/sessionHandler.py
+++ andlon/sessionHandler.py
@@ -1,19 +1,11 @@
 ##################   Filename of each session   ##################
-sessions = ['HH20150527 Nasl II - $0.20-$0.40 - USD 8-Game',
-			'HH20150527 Nasl II - $0.20-$0.40 - USD 8-Game (copy)'
-																	]
-##################   return list with all sessions in plotable format   ##################
-def read_session_from_file(file_name):
-	file = open(file_name,'r')
-	session = [10.0]
-	session += [ float( line[17:22] ) for line in file if line[8:17] == 'andlon ($' ]
-	file.close()
-	return session
+sessions = ['session']
+##################   return list with all sessions   ##################
 
 def get_sessions():
-	plotable_sessions = []
+	all_sessions = []
 	for session in sessions:
-		plotable_sessions += read_session_from_file(session+'.txt')
-	print len(plotable_sessions), 'hender over', len(sessions), 'sesjoner'
-	return plotable_sessions
+		all_sessions.append( session + '.txt' )
+	#print len(all_sessions), 'hender over', len(sessions), 'sesjoner'
+	return all_sessions
 
